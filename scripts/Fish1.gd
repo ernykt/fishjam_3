b8e71e66d38c8f502490dd879503acc73bba875d
Musa Eren YAKUT <m.eren.yakut@outlook.com>
2024-04-06 16:56:45 +0300

    progress bar

--- scripts/Fish1.gd
+++ scripts/Fish1.gd
@@ -1,8 +1,9 @@
 extends CharacterBody2D
 
 @onready var text_box_scene = preload("res://scenes/text_box.tscn")
+@onready var progress_bar = $TextureProgressBar
 
-var dialog_options: Array[String] = ["test", "eren", "doki", "amk neden böyle oluyor"]
+var dialog_options: Array[String] = ["test", "eren", "doki", "neden böyle oluyor"]
 
 var SPEED = 300.0
 var text_box
@@ -34,3 +35,13 @@ func _show_text_box():
 	text_box.display_text(dialog_options[random_text])
 	
 
+
+
+func _on_progress_timer_timeout():
+	progress_bar.value -= 10
+	if progress_bar.value <= 0:
+		var tween_mod = get_tree().create_tween()
+		var tween = get_tree().create_tween()
+		tween.tween_property(self, "position", position - Vector2(0, 25), 0.3)
+		tween_mod.tween_property(self, "modulate:a", 0, 0.3)
+		tween_mod.tween_callback(queue_free)
